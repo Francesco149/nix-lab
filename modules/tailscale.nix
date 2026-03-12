66bb2ade1a66487d7f5542ca5b630c03593608f6
headpats <loli@headpats.uk>
2026-03-12 03:17:53 +0100

    fix: local machines should not accept ts dns

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -3,6 +3,9 @@
     enable = true;
     extraUpFlags = [
       "--login-server=https://hs.headpats.uk"
+      "--accept-dns=false"
+      # local machines have no reason to use the tailscale
+      # dns and it causes hangs in resolution sometimes.
     ];
   };
 }
