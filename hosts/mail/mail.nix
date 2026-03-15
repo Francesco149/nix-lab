d54a3b9f3664aeeb76556956180ff57587c676c4
headpats <loli@headpats.uk>
2026-03-15 07:41:08 +0100

    fix: tlsrpt is an attrset that contains enable

--- hosts/mail/mail.nix
+++ hosts/mail/mail.nix
@@ -35,7 +35,7 @@
     # help improve email security across the internet by sending feedback on
     # authentication failures, spoofing attempts, and TLS encryption issues.
     dmarcReporting.enable = true;
-    tlsrpt = true;
+    tlsrpt.enable = true;
 
     # since 26.05 we explicitly use nginx for the cert
     x509.useACMEHost = config.mailserver.fqdn;
